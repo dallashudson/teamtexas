8e7ef6f7b5039def706037bed70b01abdd0f5fb4
dallashudson <dallashyz@gmail.com>
2021-07-23 16:03:27 -0500

    Current front end

--- ml-fire-detection/src/Components/Header.js
+++ ml-fire-detection/src/Components/Header.js
@@ -10,6 +10,7 @@ class Header extends Component {
     const github = this.props.data.github;
     const name = this.props.data.name;
     const description = this.props.data.description;
+    const image ="";
 
     return (
       <header id="home">
